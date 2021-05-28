f5c10060da026de468d2b26aba7ea493fd66b443
Samuel Hinkhouse <hinkhouse.samuel@gmail.com>
2021-05-27 19:56:06 -0500

    hook up contact service

--- src/app/pages/contacts/contacts.component.ts
+++ src/app/pages/contacts/contacts.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ContactDetailsComponent } from 'src/app/core/components/contact-details/contact-details.component';
+import { Contact } from 'src/app/core/models/contact.model';
+import { ContactsService } from 'src/app/core/services/contacts.service';
 
 @Component({
-  selector: 'app-contacts',
-  templateUrl: './contacts.component.html',
-  styleUrls: ['./contacts.component.scss']
+    selector: 'app-contacts',
+    templateUrl: './contacts.component.html',
+    styleUrls: ['./contacts.component.scss'],
 })
 export class ContactsComponent implements OnInit {
+    public contactList: Contact[];
 
-  constructor() { }
+    constructor(private contacts: ContactsService, private dialog: MatDialog) {}
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.getContacts();
+    }
 
+    getContacts() {
+        this.contactList = this.contacts.getContacts() as Contact[];
+    }
+
+    openContactDetails(contactDetails: Contact) {
+        this.dialog.open(ContactDetailsComponent, {
+            minWidth: '400px',
+            data: {
+                contactDetails
+            }
+        })
+    }
 }
--- src/app/pages/contacts/contacts.module.ts
+++ src/app/pages/contacts/contacts.module.ts
@@ -3,10 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { ContactsRoutingModule } from './contacts-routing.module';
 import { ContactsComponent } from './contacts.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatDialogModule } from '@angular/material/dialog';
+import { ContactDetailsModule } from 'src/app/core/components/contact-details/contact-details.module';
 
 @NgModule({
     declarations: [ContactsComponent],
-    imports: [CommonModule, ContactsRoutingModule],
+    imports: [
+        CommonModule,
+        ContactsRoutingModule,
+        FlexLayoutModule,
+        MatDialogModule,
+        ContactDetailsModule
+    ],
     exports: [ContactsComponent],
 })
 export class ContactsModule {}
