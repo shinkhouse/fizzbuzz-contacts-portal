e3fa6b73055019bc8a2df0a519e3596a87c6669c
Samuel Hinkhouse <shinkhouse@nfsmith.com>
2021-12-21 14:56:32 -0600

    create temporary empty component for pages still in progress

---
+++ src/app/core/components/empty-listing/empty-listing.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EmptyListingComponent } from './empty-listing.component';
+
+describe('EmptyListingComponent', () => {
+  let component: EmptyListingComponent;
+  let fixture: ComponentFixture<EmptyListingComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ EmptyListingComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(EmptyListingComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/core/components/empty-listing/empty-listing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-empty-listing',
+  templateUrl: './empty-listing.component.html',
+  styleUrls: ['./empty-listing.component.scss']
+})
+export class EmptyListingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
